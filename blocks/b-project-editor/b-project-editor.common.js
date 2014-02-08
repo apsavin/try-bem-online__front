a8405a9ed0929d73a186ba3ddc8f4d18d345f6b1
Alexander Savin <box.for.postman@gmail.com>
2014-02-08 18:04:56 +0400

    error handling improved

--- blocks/b-project-editor/b-project-editor.common.js
+++ blocks/b-project-editor/b-project-editor.common.js
@@ -1,13 +1,25 @@
 BN.addDecl('b-project-editor')
     .dataTemplate(function (ctx) {
-        return BN('i-projects-api').get('clones/' + ctx.param('project')).then(function (data) {
-            ctx.param('data', data);
-            return Vow.fulfill();
-        });
+        return BN('i-projects-api').get('clones/' + ctx.param('project'))
+            .then(function (data) {
+                ctx.param('data', data);
+            })
+            .fail(function (err) {
+                ctx.param('data', err);
+            });
     })
     .blockTemplate(function (ctx) {
         var data = ctx.json().data,
             content = data.content;
+
+        if (!content) {
+            ctx.content({
+                block: 'b-project-error',
+                content: data.status
+            });
+            return;
+        }
+
         if (Array.isArray(content)) {
             content = {
                 block: 'b-project-dir',
