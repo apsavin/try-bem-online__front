7d40ef27885b00f859238cc1292db593682c88ca
Alexander Savin <box.for.postman@gmail.com>
2014-03-09 16:16:00 +0400

    page-project fast updates

--- blocks/page-project/page-project.common.js
+++ blocks/page-project/page-project.common.js
@@ -1,12 +1,16 @@
 BN.addDecl('page-project', 'page', {
     route: /^\/clones\/([\w-]+)\/?(.*)?\/$/
 }).staticProp({
+
+    /**
+     * @param {Array} matchers
+     * @returns {Vow.Promise}
+     */
     init: function (matchers) {
 
         BN('i-page').setTitle('Your project');
 
-        var projectId = matchers[1],
-            path = matchers[2] || '';
+        var projectId = matchers[1];
 
         return this.out([
             {
@@ -17,15 +21,7 @@ BN.addDecl('page-project', 'page', {
             {
                 block: 'b-project-layout',
                 content: [
-                    {
-                        block: 'b-project-editor',
-                        projectId: projectId,
-                        path: path,
-                        mix: {
-                            block: 'b-project-layout',
-                            elem: 'content'
-                        }
-                    },
+                    this._getProjectEditorJson(matchers),
                     {
                         block: 'b-project-layout',
                         elem: 'sidebar',
@@ -37,5 +33,41 @@ BN.addDecl('page-project', 'page', {
                 ]
             }
         ]);
+    },
+
+    /**
+     * @param {Array} matchers
+     * @returns {Object}
+     * @private
+     */
+    _getProjectEditorJson: function (matchers) {
+        var projectId = matchers[1],
+            path = matchers[2] || '';
+        return {
+            block: 'b-project-editor',
+            projectId: projectId,
+            path: path,
+            mix: {
+                block: 'b-project-layout',
+                elem: 'content'
+            }
+        };
+    },
+
+    /**
+     * @param {Array} matchers
+     * @returns {Vow.Promise}
+     */
+    update: function (matchers) {
+        return this.html(this._getProjectEditorJson(matchers)).then(function (html) {
+            try {
+                BEM.DOM.update(
+                    jQuery('.b-project-editor'),
+                    html
+                );
+            } catch (ex) { console.error(ex); }
+            jQuery('body, html').scrollTop(0);
+            return html;
+        }.bind(this));
     }
 });
